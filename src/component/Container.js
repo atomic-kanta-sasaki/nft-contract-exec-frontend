05cebc7fab0d925359728c4af5df23d46a520179
ka-sasaki-sti <ka-sasaki@sios.com>
2022-11-16 21:17:29 +0900

    NFT転送までできた

--- src/component/Container.js
+++ src/component/Container.js
@@ -2,13 +2,60 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import ImgList from './ImgList'
+import { ethers, Contract } from "ethers";
+import { useEffect, useState } from "react";
+import artifact from "../abi/nft.json";
+import axios from 'axios'
 
 export default function SimpleContainer() {
+    // NFT所有者情報
+    // NFTのtokenId -> meta data -> image url
+    const [nftSalePageInfo, setNftSalePageInfo]  = useState({
+        tokenId: '',
+        owner: '',
+        imageUri:'',
+      });
+    const [posts, setPosts] = useState([])
+    // metamaskを介してネットワークノードとの通信をするオブジェクトを作成する
+    const contractAddress = "0x04c89607413713ec9775e14b954286519d836fef";
+    // アドレス、ABI, プロバイダを指定してコントラクトオブジェクトを作成
+    // コントラクトの状態を変化させる(gas代が必要な）操作をするためには場合はSignerを与える必要がある
+    const provider = new ethers.providers.JsonRpcProvider();
+    const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+    const signer = provider.getSigner();
+    const nftContract = contract.connect(signer);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            //owner of の呼出し
+            // 配列にする
+            const owner = await nftContract.ownerOf(3)
+            const tokenId = 3
+            const metaDataUri = await nftContract.tokenURI(3)
+            const metaData = await axios.get(metaDataUri)
+              .then(res => {
+                  // const str = res.data.replace('https://ipfs.io/ipfs/', '')
+                  // const json = JSON.parse(str)
+                  return res.data
+              })
+              .catch(e => {
+                console.log(e)
+              })
+              // console.log('-----------------------------')
+              // console.log(metaData)
+              // console.log('-----------------------------')
+            setNftSalePageInfo({tokenId: tokenId, owner: owner, imageUri: metaData.image})
+        };
+        fetchData().catch((e) => console.log(e));
+    }, []);
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container fixed>
-        <ImgList />
+        <ImgList
+          { ...nftSalePageInfo }
+        />
       </Container>
     </React.Fragment>
   );
--- src/component/ImgList.js
+++ src/component/ImgList.js
@@ -7,7 +7,8 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import style from 'styled-components';
 import Button from '@mui/material/Button';
-
+import artifact from "../abi/nft.json";
+import { ethers, Contract } from "ethers";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,8 +25,26 @@ const DisplayFlex = style.div`
 const MarginTop = style.div`
   margin-top:60%
 `
-
-export default function StandardImageList() {
+ // metamaskを介してネットワークノードとの通信をするオブジェクトを作成する
+ const contractAddress = "0x04c89607413713ec9775e14b954286519d836fef";
+ // アドレス、ABI, プロバイダを指定してコントラクトオブジェクトを作成
+ // コントラクトの状態を変化させる(gas代が必要な）操作をするためには場合はSignerを与える必要がある
+ const provider = new ethers.providers.JsonRpcProvider();
+ const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+ const signer = provider.getSigner();
+ const nftContract = contract.connect(signer);
+// イベントハンドラ
+// safeTranferを実行する
+// 購入者がデポジットする
+// transfer購入者 -> 出品者
+// withdraw出品者
+const handleClick = async(e) => {
+  console.log(nftContract)
+  const kk = await nftContract['safeTransferFrom(address,address,uint256)'](e.from, e.to, e.tokenId)
+  console.log(kk)
+}
+const StandardImageList = (props) => {
+  console.log(props)
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
@@ -37,17 +56,17 @@ export default function StandardImageList() {
                   <DisplayFlex>
                     <div>
                       <img
-                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${props.imageUri}?w=164&h=164&fit=crop&auto=format`}
+                        srcSet={`${props.imageUri}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                         alt={item.title}
                         loading="lazy"
                       />
                     </div>
                     <div>
-                      所有者: hogehogehoge <br />
+                      所有者: {props.owner} <br />
                       値段: 1ETH <br />
                       <MarginTop>
-                        <Button variant="contained">Contained</Button>
+                        <Button variant="contained" onClick={() => handleClick({from:'0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', to:'0x70997970C51812dc3A010C7d01b50e0d17dc79C8', tokenId:3})}>Contained</Button>
                       </MarginTop>
                     </div>
                   </DisplayFlex>
@@ -58,7 +77,6 @@ export default function StandardImageList() {
         ))}
       </Grid>
     </Box>
-
   );
 }
 
@@ -66,49 +84,64 @@ const itemData = [
   {
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
+    owner: 'owner 01',
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     title: 'Burger',
+    owner: 'owner 01',
   },
   {
     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
     title: 'Camera',
+    owner: 'owner 01',
   },
   {
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
+    owner: 'owner 01',
+
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
+    owner: 'owner 01',
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
+    owner: 'owner 01',
   },
   {
     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
     title: 'Basketball',
+    owner: 'owner 02',
   },
   {
     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
     title: 'Fern',
+    owner: 'owner 02',
   },
   {
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
     title: 'Mushrooms',
+    owner: 'owner 02',
   },
   {
     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
     title: 'Tomato basil',
+    owner: 'owner 02',
   },
   {
     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
     title: 'Sea star',
+    owner: 'owner 02',
   },
   {
     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
     title: 'Bike',
+    owner: 'owner 02',
   },
 ];
+
+export default StandardImageList;
