dbe895053c53123ada620647ec7521add249138d
ka-sasaki-sti <ka-sasaki@sios.com>
2022-11-17 18:39:43 +0900

    nft 動的制御

--- src/component/Container.js
+++ src/component/Container.js
@@ -10,41 +10,38 @@ import axios from 'axios'
 export default function SimpleContainer() {
     // NFT所有者情報
     // NFTのtokenId -> meta data -> image url
-    const [nftSalePageInfo, setNftSalePageInfo]  = useState({
-        tokenId: '',
-        owner: '',
-        imageUri:'',
-      });
-    const [posts, setPosts] = useState([])
+    const [nftSalePageInfo, setNftSalePageInfo]  = useState([]);
     // metamaskを介してネットワークノードとの通信をするオブジェクトを作成する
-    const contractAddress = "0x04c89607413713ec9775e14b954286519d836fef";
+    const contractAddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
     // アドレス、ABI, プロバイダを指定してコントラクトオブジェクトを作成
     // コントラクトの状態を変化させる(gas代が必要な）操作をするためには場合はSignerを与える必要がある
     const provider = new ethers.providers.JsonRpcProvider();
     const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
     const signer = provider.getSigner();
     const nftContract = contract.connect(signer);
+    const tokenIdList = [1,2,3]
+    const info = []
 
     useEffect(() => {
         const fetchData = async() => {
             //owner of の呼出し
             // 配列にする
-            const owner = await nftContract.ownerOf(3)
-            const tokenId = 3
-            const metaDataUri = await nftContract.tokenURI(3)
-            const metaData = await axios.get(metaDataUri)
-              .then(res => {
-                  // const str = res.data.replace('https://ipfs.io/ipfs/', '')
-                  // const json = JSON.parse(str)
-                  return res.data
-              })
-              .catch(e => {
-                console.log(e)
-              })
-              // console.log('-----------------------------')
-              // console.log(metaData)
-              // console.log('-----------------------------')
-            setNftSalePageInfo({tokenId: tokenId, owner: owner, imageUri: metaData.image})
+            tokenIdList.map(async(id) => {
+              const owner = await nftContract.ownerOf(id)
+              const tokenId = id
+              const metaDataUri = await nftContract.tokenURI(id)
+              const metaData = await axios.get(metaDataUri)
+                .then(res => {
+                    return res.data
+                })
+                .catch(e => {
+                  console.log(e)
+                })
+              info.push({owner:owner, tokenId:tokenId, imageUri: metaData.image})
+            })
+            console.log(info)
+            setNftSalePageInfo((prevState) => ([ ...prevState, info ]));
+
         };
         fetchData().catch((e) => console.log(e));
     }, []);
